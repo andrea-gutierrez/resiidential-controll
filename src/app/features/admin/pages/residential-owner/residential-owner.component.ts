ed8adc072b79389ad7f9082ac9617cac1968f0a0
andrea-gutierrez <andreguti93@gmail.com>
2024-09-29 14:30:04 +0800

    migrated: It was added the class TableBase in the residential-owner component

--- src/app/features/admin/pages/residential-owner/residential-owner.component.ts
+++ src/app/features/admin/pages/residential-owner/residential-owner.component.ts
@@ -1,15 +1,15 @@
 import {Component, inject, OnInit} from '@angular/core';
 
 import {
-  MatTableDataSource,
   MatTableModule
 } from "@angular/material/table";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
 
 import {ResidentialOwnerService} from "../../services/residential-owner.service";
 import {ResidentialOwner} from "../../interfaces/residentialOwner.interface";
-import {MatButtonModule} from "@angular/material/button";
+import {TableBase} from "../../../../shared/base/table.base";
 
 @Component({
   selector: 'app-residential-owner',
@@ -20,54 +20,33 @@ import {MatButtonModule} from "@angular/material/button";
   templateUrl: './residential-owner.component.html',
   styleUrl: './residential-owner.component.scss'
 })
-export class ResidentialOwnerComponent implements OnInit {
+export class ResidentialOwnerComponent extends TableBase<ResidentialOwner> implements OnInit {
   private residentialOwnerService = inject(ResidentialOwnerService);
+  private filterList: string[] = ['name', 'email', 'document', 'tower'];
 
   public displayedColumns: string[] = ['Nombre', 'Documento', 'Email', 'Torre', 'Acciones'];
 
-  dataSource: MatTableDataSource<ResidentialOwner> = new MatTableDataSource();
-
   constructor() {
+    super();
   }
 
   ngOnInit() {
     this.loadResidentialOwner();
   }
 
-  loadResidentialOwner(): void {
+  private loadResidentialOwner(): void {
     this.residentialOwnerService.getAll().subscribe({
       next: (data: ResidentialOwner[]) => {
         this.setDataSource(data);
+        this.setFilterBy(this.filterList);
       }
     });
   }
 
-  setDataSource(ownerList: ResidentialOwner[]): void {
-    this.dataSource.data = ownerList;
-    this.dataSource._updateChangeSubscription();
-    this.dataSource.filterPredicate = (data, filter: string): boolean => {
-      return data.name.toLowerCase().includes(filter)
-        || data.email.toLowerCase().includes(filter)
-        || data.document.toLowerCase().includes(filter)
-        || data.tower.toLowerCase().includes(filter);
-    };
-  }
-
-  applyFilter(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-
-  delete(residentialOwner: ResidentialOwner): void {
+  public onDelete(residentialOwner: ResidentialOwner): void {
     this.residentialOwnerService.deleteById(residentialOwner.document).subscribe({
       next: () => {
-        const index = this.dataSource.data.indexOf(residentialOwner);
-        this.dataSource.data.splice(index, 1);
-        this.dataSource._updateChangeSubscription();
+        this.deleteRow(residentialOwner);
       }
     })
   }
